53f4bd718e3ac358afcd0af82bf5449c399ea64b
bombik815 <58844959+bombik815@users.noreply.github.com>
2025-10-20 17:37:16 +0300

    add in app view function docs and redocs

--- url-shortener/main.py
+++ url-shortener/main.py
@@ -1,7 +1,30 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
 
 app = FastAPI(
     title="URL Shortener",
     description="URL Shortener",
 )
+
+
+@app.get("/")
+async def read_root(
+    request: Request,
+    name: str = "Shortener",
+):
+    """
+    Корневой view - возвращает JSON со ссылками на документацию
+    """
+    docs_url = request.url.replace(
+        path="/docs",
+        query="",
+    )
+    redoc_url = request.url.replace(
+        path="/redoc",
+        query="",
+    )
+
+    return {
+        "message": f"Welcome to the URL {name} API",
+        "docs": str(docs_url),
+        "redoc": str(redoc_url),
+    }
