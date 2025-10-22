202415a840cb72666caf7e5ae5e488e1ff92aa8a
bombik815 <58844959+bombik815@users.noreply.github.com>
2025-10-22 16:25:24 +0300

    Add schemas for short_url.py

---
+++ url-shortener/schemas/short_url.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, HttpUrl
+
+
+class ShortUrlBase(BaseModel):
+    target_url: str
+    slug: str
+
+
+class ShortUrl(ShortUrlBase):
+    """
+    Модель сокращенной ссылки
+    """
