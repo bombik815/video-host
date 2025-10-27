118e84ad8d16b43ae32bbea870c63ef9528bf3f3
bombik815 <58844959+bombik815@users.noreply.github.com>
2025-10-27 16:45:11 +0300

    updated

--- url-shortener/api/api_v1/__init__.py
+++ url-shortener/api/api_v1/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from .short_urls.views import router as short_urls_router
+from .movies.views import router as movies_router
 
 router = APIRouter(
     prefix="/v1",
 )
 
 router.include_router(short_urls_router)
+router.include_router(movies_router)
