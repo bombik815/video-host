fcae78ad2a61da70a7d1bb7f65260874bc118db1
bombik815 <58844959+bombik815@users.noreply.github.com>
2025-11-01 12:09:55 +0300

    Add POST endpoint for creating short URLs with form validation

--- url-shortener/api/api_v1/short_urls/views.py
+++ url-shortener/api/api_v1/short_urls/views.py
@@ -1,6 +1,14 @@
 from typing import Annotated
 
-from fastapi import Depends, APIRouter
+from annotated_types import Len
+
+from fastapi import (
+    Depends,
+    APIRouter,
+    status,
+    Form,
+)
+from pydantic import AnyHttpUrl
 
 from schemas.short_url import ShortUrl
 
@@ -23,6 +31,21 @@ router = APIRouter(prefix="/short-urls", tags=["Short URLs"])
 @router.get("/", response_model=list[ShortUrl])
 def read_short_urls_list():
     return SHORT_URLS
+
+
+@router.post("/", response_model=ShortUrl, status_code=status.HTTP_201_CREATED)
+def create_short_url(
+    target_url: Annotated[AnyHttpUrl, Form()],
+    slug: Annotated[
+        str,
+        Len(min_length=3, max_length=10),
+        Form(),
+    ],
+):
+    return ShortUrl(
+        target_url=target_url,
+        slug=slug,
+    )
 
 
 """
