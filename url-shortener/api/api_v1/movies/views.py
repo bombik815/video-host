199d42d10ea251ba5c1768ae1b31b1ced0edf4f9
bombik815 <58844959+bombik815@users.noreply.github.com>
2025-11-01 12:10:06 +0300

    Add POST endpoint for creating movies with form validation

--- url-shortener/api/api_v1/movies/views.py
+++ url-shortener/api/api_v1/movies/views.py
@@ -1,6 +1,16 @@
+import datetime
+
 from typing import Annotated
 
-from fastapi import Depends, APIRouter
+from annotated_types import Len
+
+import status
+from fastapi import (
+    Depends,
+    APIRouter,
+    status,
+    Form,
+)
 
 from .crud import MOVIES
 from schemas.movie import Movie
@@ -21,6 +31,15 @@ router = APIRouter(prefix="/movies", tags=["Movies"])
 @router.get("/", response_model=list[Movie])
 def get_movies() -> list[Movie]:
     return MOVIES
+
+
+@router.post("/", response_model=Movie, status_code=status.HTTP_201_CREATED)
+def creat_movie(
+    title: Annotated[str, Form()],
+    description: Annotated[str, Len(min_length=3, max_length=100), Form()],
+    year: Annotated[int, Form(min_value=1900, max_value=datetime.date.today().year)],
+):
+    return Movie(title=title, description=description, year=year)
 
 
 """
