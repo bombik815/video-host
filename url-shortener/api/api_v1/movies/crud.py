7f49d2b44079992ba0f7606877df319c7be77f92
bombik815 <58844959+bombik815@users.noreply.github.com>
2025-11-25 15:25:12 +0300

    Refactor movie storage to use class-based CRUD operations with slug-based lookup

--- url-shortener/api/api_v1/movies/crud.py
+++ url-shortener/api/api_v1/movies/crud.py
@@ -1,22 +1,75 @@
-from schemas.movie import Movie
+from pydantic import BaseModel
 
-MOVIES = [
-    Movie(
+from schemas.movie import Movie, MovieCreate
+
+
+class MovieStorage(BaseModel):
+    slug_to_movie: dict[str, Movie] = {}
+
+    """
+    Возвращает список всех сохраненных объектов Movie.
+
+    Возвращает:
+        list[Movie]: список всех сохраненных объектов Movie
+    """
+
+    def get(self) -> list[Movie]:
+        return list(self.slug_to_movie.values())
+
+    """
+    Возвращает объект Movie по его slug.
+
+    Параметры:
+        slug (str): slug объекта Movie
+
+    Возвращает:
+        Movie | None: объект Movie если найден, иначе None
+    """
+
+    def get_by_slug(self, slug: str) -> Movie | None:
+        return self.slug_to_movie.get(slug)
+
+    """
+    Создает объект Movie на основе переданных параметров.
+
+    Параметры:
+        movie_create (MovieCreate): объект MovieCreate, содержащий параметры для создания Movie
+
+    Возвращает:
+        Movie: созданный объект Movie
+    """
+
+    def create(self, movie_create: MovieCreate) -> Movie:
+        movie = Movie(
+            **movie_create.model_dump(),
+        )
+        self.slug_to_movie[movie.slug] = movie
+        return movie
+
+
+storage = MovieStorage()
+
+storage.create(
+    MovieCreate(
         slug="godfather",
         title="Крестный отец",
         description="Криминальная драма о семье мафиози Корлеоне",
         year=1972,
-    ),
-    Movie(
+    )
+)
+storage.create(
+    MovieCreate(
         slug="shawshank-redemption",
         title="Побег из Шоушенка",
         description="Драма о несправедливо осужденном банкире",
         year=1994,
-    ),
-    Movie(
+    )
+)
+storage.create(
+    MovieCreate(
         slug="dark-knight",
         title="Темный рыцарь",
         description="Бэтмен сражается с Джокером",
         year=2008,
-    ),
-]
+    )
+)
