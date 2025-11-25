9e63042903d8908ec1e8073a19aa59037c93ffd0
bombik815 <58844959+bombik815@users.noreply.github.com>
2025-11-25 15:24:28 +0300

    Refactor short URL storage to use class-based CRUD operations with slug-based lookup

--- url-shortener/api/api_v1/short_urls/crud.py
+++ url-shortener/api/api_v1/short_urls/crud.py
@@ -1,13 +1,64 @@
-from schemas.short_url import ShortUrl
+from pydantic import BaseModel
+
+from schemas.short_url import ShortUrl, ShortUrlCreate
 
 
-SHORT_URLS = [
-    ShortUrl(
+class ShortUrlStorage(BaseModel):
+    slug_to_short_url: dict[str, ShortUrl] = {}
+
+    """
+    Возвращает список всех сохраненных объектов ShortUrl.
+    
+    Возвращает:
+        list[ShortUrl]: список всех сохраненных объектов ShortUrl
+    """
+
+    def get(self) -> list[ShortUrl]:
+        return list(self.slug_to_short_url.values())
+
+    """
+    Возвращает объект ShortUrl по его slug.
+
+    Параметры:
+        slug (str): slug объекта ShortUrl
+
+    Возвращает:
+        ShortUrl | None: объект ShortUrl если найд��н, иначе None
+    """
+
+    def get_by_slug(self, slug: str) -> ShortUrl | None:
+        return self.slug_to_short_url.get(slug)
+
+    """
+    Создает объект ShortUrl на основе переданных параметров.
+
+    Параметры:
+        short_url_create (ShortUrlCreate): объект ShortUrlCreate, содержащий параметры для создания ShortUrl
+
+    Возвращает:
+        ShortUrl: созданный объект ShortUrl
+    """
+
+    def create(self, short_url_create: ShortUrlCreate) -> ShortUrl:
+        short_url = ShortUrl(
+            **short_url_create.model_dump(),
+        )
+        self.slug_to_short_url[short_url.slug] = short_url
+        return short_url
+
+
+storage = ShortUrlStorage()
+
+
+storage.create(
+    ShortUrlCreate(
         target_url="https://example.com",
         slug="example",
-    ),
-    ShortUrl(
+    )
+)
+storage.create(
+    ShortUrlCreate(
         target_url="https://google.com",
         slug="search",
     ),
-]
+)
