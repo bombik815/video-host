af34132b2d51194ecbf393400747590773dd725e
bombik815 <58844959+bombik815@users.noreply.github.com>
2025-11-25 14:15:17 +0300

    Add validation schemas for movie and short URL creation

--- url-shortener/schemas/movie.py
+++ url-shortener/schemas/movie.py
@@ -1,4 +1,10 @@
-from pydantic import BaseModel
+import datetime
+import random
+from typing import Annotated
+
+from annotated_types import Len
+from fastapi import Form
+from pydantic import BaseModel, Field
 
 
 class MovieBase(BaseModel):
@@ -7,9 +13,19 @@ class MovieBase(BaseModel):
     year: int
 
 
+class MovieCreate(MovieBase):
+    """
+    Модель создания фильма
+    """
+
+    title: Annotated[str, Len(min_length=3, max_length=100)]
+    description: Annotated[str, Len(min_length=3, max_length=100)]
+    year: Annotated[int, Form(min_value=1900, max_value=datetime.date.today().year)]
+
+
 class Movie(MovieBase):
     """
     Модель фильма
     """
 
-    # id: int
+    id: int = Field(default_factory=lambda: random.randint(1_000_000, 9_999_999))
