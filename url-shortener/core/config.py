0d3889f679cbef3ddc710bba643c93eac710161f
bambulya <bombik815@gmail.com>
2026-04-29 13:07:24 +0300

    refact code

--- url-shortener/core/config.py
+++ url-shortener/core/config.py
@@ -12,7 +12,7 @@ LOG_FORMAT: str = (
     "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
 )
 
-USERS_DB: dict[[str, str]] = {
+USERS_DB: dict[str, str] = {
     # username: password
     "sam": "password",
     "bob": "qwerty",
