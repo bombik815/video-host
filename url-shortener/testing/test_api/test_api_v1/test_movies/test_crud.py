c98f43b1050df3037e6edcd4e2fdcc75a4da88f6
bambulya <bombik815@gmail.com>
2026-06-09 15:20:41 +0300

    Pre-setup before all tests and cleaning up after yourself

--- url-shortener/testing/test_api/test_api_v1/test_movies/test_crud.py
+++ url-shortener/testing/test_api/test_api_v1/test_movies/test_crud.py
@@ -1,3 +1,4 @@
+from typing import ClassVar
 from unittest import TestCase
 
 from api.api_v1.movies.crud import storage
@@ -12,25 +13,26 @@ from testing import ensure_testing_environment
 ensure_testing_environment()
 
 
+def create_movie() -> Movie:
+    """Создаёт и возвращает Фильм для тестов."""
+
+    create_movie_in = MovieCreate(
+        title="Some title",
+        description="Some description",
+        year=2005,
+        slug="some-slug",
+    )
+    return storage.create(create_movie_in)
+
+
 class MovieStorageUpdateTestCase(TestCase):
     def setUp(self) -> None:
         """Создаёт тестовый Фильм перед каждым тестом."""
-        self.movie = self.create_movie()
+        self.movie = create_movie()
 
     def tearDown(self) -> None:
         """Удаляет тестовый Фильм после каждого теста."""
         storage.delete(self.movie)
-
-    def create_movie(self) -> Movie:
-        """Создаёт и возвращает Фильм для тестов."""
-
-        create_movie_in = MovieCreate(
-            title="Some title",
-            description="Some description",
-            year=2005,
-            slug="some-slug",
-        )
-        return storage.create(create_movie_in)
 
     def test_update(self) -> None:
         """Проверяет полное обновление описания фильма."""
@@ -78,3 +80,34 @@ class MovieStorageUpdateTestCase(TestCase):
         self.assertEqual(old_title, updated_movie.title)
         self.assertEqual(old_year, updated_movie.year)
         self.assertEqual(old_slug, updated_movie.slug)
+
+
+class MovieStorageGetMoviesTestCase(TestCase):
+
+    MOVIES_COUNT = 3
+    movies: ClassVar[list[Movie]] = []
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.movies = [create_movie() for _ in range(cls.MOVIES_COUNT)]
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        for movie in cls.movies:
+            storage.delete(movie)
+
+    def test_get_list(self) -> None:
+        movies = storage.get()
+        expected_movies = {mv.slug for mv in self.movies}
+        slugs = {mv.slug for mv in movies}
+
+        self.assertEqual(expected_movies, slugs)
+
+    def test_get_by_slug(self) -> None:
+        for movie in self.movies:
+            with self.subTest(
+                slug=movie.slug,
+                msg=f"Validate can slug {movie.slug!r}",
+            ):
+                db_short_url = storage.get_by_slug(movie.slug)
+                self.assertEqual(movie, db_short_url)
